81150c135b0a7bfed52b0c35858c1eaa775fd9b3
danyelo arotinco <yoelitosagitario@gmail.com>
2020-06-30 16:31:50 -0500

    Prog001 - prog004 Listo

---
+++ src/estructuras/prog004.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estructuras;
+
+/**
+ *
+ * @author YOEL
+ */
+public class prog004 {
+    public static void main(String[] args) {
+        int num =1;
+        String mensaje = "Cristo viene pronto - Prepàrate";
+        while (num<=10) { 
+            System.out.println(mensaje);
+            num++;
+            
+            
+        }
+    }
+    
+}

d0f978920b39d85b5735a9921c3712490edaae55
danyelo arotinco <yoelitosagitario@gmail.com>
2020-07-07 14:13:41 -0500

    Prog 007 - método vacío

---
+++ src/Modularidad/Prog007.java
@@ -0,0 +1,30 @@
+
+package Modularidad;
+
+//@author YOEL
+ 
+public class Prog007 {
+    // Método vacío ...
+    // Vacío porque dentro del paréntesis está vacío
+    public static void saludo(){
+        System.out.println("Bienvenido al sistema");
+    } 
+    
+    public static void despedida() {
+        System.out.println("Espero Verte Pronto");
+        
+    }
+    public static void mensaje() {
+        System.out.println("En todo tiempo ama el amigo y es como hermano "
+                + "en tiempo de angustia Prov. 17:17");
+        
+    }
+   
+    
+    public static void main(String[] args) {
+        saludo(); 
+        mensaje();
+        despedida();
+    }
+    
+}

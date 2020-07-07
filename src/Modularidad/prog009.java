3e65e3f5ecc513775e03f52889d0c3309ab30b09
danyelo arotinco <yoelitosagitario@gmail.com>
2020-07-07 15:33:54 -0500

    Prog 009 - método que devuelve parámetros

---
+++ src/Modularidad/prog009.java
@@ -0,0 +1,22 @@
+
+package Modularidad;
+
+/**
+ *
+ * @author YOEL
+ */
+public class prog009 {
+    //método que devuelven valores
+    public static String miuniversidad() {
+        String universidad= "Universidad Nacional Tecnológica de Lima Sur";
+        return universidad;
+        
+    }
+    public static void main(String[] args) {
+        System.out.println(miuniversidad());
+        System.out.println(miuniversidad().toUpperCase());
+        
+                    
+    }
+    
+}
